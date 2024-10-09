c02d66927c84111f1688f3b2e25f7bd024fa03b1
Mike Miller <mgmiller@keller-na.com>
2024-10-09 15:53:42 -0400

    asdf

--- src/lib/outline.cc
+++ src/lib/outline.cc
@@ -317,7 +317,7 @@ void Outline::fillHeaderFooterParms(int page, QHash<QString, QString> & parms, c
 	parms["webpage"] = ps.page;
 	parms["section" ] = d->hfCache[0][page]?d->hfCache[0][page]->value:QString("");
 
-	//parms["subsection" ] = d->hfCache[1][page]?d->hfCache[1][page]->value:QString("");
+	// parms["subsection" ] = d->hfCache[1][page]?d->hfCache[1][page]->value:QString("");
 	parms["subsection"] = d->hfCache[1][page] ? SanitizeTitle(d->hfCache[1][page]->value) : QString("");
 	parms["subsubsection" ] = d->hfCache[2][page]?d->hfCache[2][page]->value:QString("");
 }
